3ecd0734d13ec88decaa80eb0034bbc82326f64b
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2025-04-16 02:34:00 -0500

    Enable dot projector for OAK-D Pro devices

--- spectacle.py
+++ spectacle.py
@@ -20,6 +20,13 @@ def signal_handler(sig, frame):
 pipeline = depthai.Pipeline()
 config = spectacularAI.depthai.Configuration()
 
+epilog = """
+Modes:
+test: Run a NT4 server for debugging
+sim: Connect to simulation NT4 server
+robot: Connect to robot NT4 server
+"""
+
 # Main function
 if __name__ == "__main__":
   # Bind SIGINT handler
@@ -27,13 +34,14 @@ if __name__ == "__main__":
 
   # Init argparse
   parser = argparse.ArgumentParser(
-                    prog='Spectacle',
-                    description='Publish pose data from Spectacular AI',
-                    epilog='')
+                    prog="Spectacle",
+                    description="Publish pose data from Spectacular AI",
+                    epilog=epilog,
+                    formatter_class=argparse.RawTextHelpFormatter)
 
   # Add arguments
-  parser.add_argument('--mode', required=True, choices=['test', 'sim', 'robot'])
-  parser.add_argument('--tag-map')
+  parser.add_argument("--mode", required=True, choices=['test', 'sim', 'robot'], help="select script mode")
+  parser.add_argument("--tag-map", help="path to AprilTag map JSON file")
 
   # Parse arguments
   args = parser.parse_args()
@@ -73,41 +81,47 @@ if __name__ == "__main__":
   vio_pipeline = spectacularAI.depthai.Pipeline(pipeline, config)
   device = depthai.Device(pipeline)
 
+  # Read device name
   calib = device.readCalibration()
   eeprom = calib.getEepromData()
-
   pname = eeprom.productName
   print(pname)
 
-  #check for oak d lite, spectacular does not support by default so we need to do some extra work.
+  # Check if device is OAK-D Lite,
+  # Spectacular AI does not support by default so we need to do some extra work.
   if pname == "OAK-D-LITE":
-      vio_pipeline.imuToCameraLeft = [
-          [
-              0.9993864566531208,
-              0.002608506818609768,
-              0.03492715205248295,
-              0.004358885459078838
-          ],
-          [
-              0.0033711974751103025,
-              -0.9997567647621044,
-              -0.02179555780417905,
-              0.0002560060614699508
-          ],
-          [
-              0.034861802677196824,
-              0.021899931611508897,
-              -0.9991521644421877,
-              0.0018364413451974568
-          ],
-          [
-              0.0,
-              0.0,
-              0.0,
-              1.0
-          ]
+    vio_pipeline.imuToCameraLeft = [
+      [
+        0.9993864566531208,
+        0.002608506818609768,
+        0.03492715205248295,
+        0.004358885459078838
+      ],
+      [
+        0.0033711974751103025,
+        -0.9997567647621044,
+        -0.02179555780417905,
+        0.0002560060614699508
+      ],
+      [
+        0.034861802677196824,
+        0.021899931611508897,
+        -0.9991521644421877,
+        0.0018364413451974568
+      ],
+      [
+        0.0,
+        0.0,
+        0.0,
+        1.0
       ]
-      print("Using Oak D lite imu matrix")
+    ]
+    print("Using OAK-D Lite IMU matrix")
+
+  # Check if OAK-D Pro series
+  if "OAK-D-PRO" in pname:
+    # Enable dot projector
+    device.setIrLaserDotProjectorIntensity(0.9)
 
   vio_session = vio_pipeline.startSession(device)
   print("VIO session initialized.")
